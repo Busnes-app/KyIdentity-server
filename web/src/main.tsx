e39aebe2d9614f5b41a2b359072641a1358c49cb
Yoshiofthewire <yoshi@urlxl.com>
2026-09-25 15:11:29 -0400

    consume shared ky-ui tokens
    Use the versioned ky-ui consumer copy for shared Busnes tokens and theme behavior.

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import './index.css';
+import './ky-ui/tokens.css';
+import './ky-ui/navigation.css';
 import { applyTheme, storedTheme } from './theme';
 
 applyTheme(storedTheme());
